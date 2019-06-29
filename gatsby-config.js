e1c57957bd0041a78891d5fdc696f0b0a28084e4
David Luu <luu.david.c@gmail.com>
2019-06-28 20:34:46 -0700

    Setting up package and deleting boilerplate code

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,34 +1,40 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: 'David Luu',
+    description: 'David Luu - Software Engineer, Web Developer',
+    author: '@davidcluu',
   },
   plugins: [
-    `gatsby-plugin-react-helmet`,
+    'gatsby-mdx',
     {
-      resolve: `gatsby-source-filesystem`,
+      resolve: 'gatsby-source-filesystem',
       options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
+        name: 'images',
+        path: `${__dirname}/src/images/`,
       },
     },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
+    'gatsby-plugin-emotion',
     {
-      resolve: `gatsby-plugin-manifest`,
+      resolve: 'gatsby-plugin-manifest',
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        name: 'David Luu - Software Engineer, Web Developer',
+        short_name: 'David Luu',
+        start_url: '/',
+        background_color: '#38DDD4',
+        theme_color: '#38DDD4',
+        display: 'minimal-ui',
+        icon: 'src/images/favicon.png',
       },
     },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    'gatsby-plugin-netlify',
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-sharp',
+    {
+      resolve: 'gatsby-plugin-typography',
+      options: {
+        pathToConfigModule: './src/config/typography',
+      },
+    },
+    'gatsby-transformer-sharp',
   ],
-}
+};

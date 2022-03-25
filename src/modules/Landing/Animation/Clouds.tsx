b0f248e7611a4ab326562ed1b7a8a02b1ee92904
David Luu <luu.david.c@gmail.com>
2022-03-24 22:38:43 -0700

    Animated clouds

--- src/modules/Landing/Animation/Clouds.tsx
+++ src/modules/Landing/Animation/Clouds.tsx
@@ -1,49 +1,85 @@
-import type { MotionValue } from 'framer-motion';
+import { MotionValue, useAnimation } from 'framer-motion';
 
-import { Fragment } from 'react';
+import { Fragment, useEffect } from 'react';
 import { motion } from 'framer-motion';
 import { css } from '@emotion/react';
 
-import Cloud from './svg/Cloud';
+import SvgCloud from './svg/Cloud';
 
 import { clouds as baseZIndex } from './z-indices';
 
 import useLandingScrollPercentMotionValue from '../hooks/use-landing-scroll-percent-motion-value';
 import useTweenPercentMotionValue from '../../../hooks/use-tween-percent-motion-value';
-import useMotionValueAsPercent from '../../../hooks/use-motion-value-as-percent';
 
+const TRANSLATE_RANGE = 50;
 const width = '110px';
 
 const getTop = (
   landingScrollPercent: MotionValue<number>,
-  percentRange: number[]
-) =>
-  useMotionValueAsPercent(
-    useTweenPercentMotionValue(landingScrollPercent, percentRange)
-  );
+  percentRange: string[]
+) => useTweenPercentMotionValue(landingScrollPercent, percentRange);
 
-export default () => {
+interface CloudProps {
+  index: number;
+  left: string;
+  top: string;
+}
+
+const Cloud = ({ index, left, top }: CloudProps) => {
   const landingScrollPercent = useLandingScrollPercentMotionValue();
 
-  var zIndex = baseZIndex;
+  const animation = useAnimation();
+
+  const startAnimation = async (
+    previousX: number = 0,
+    previousY: number = 0
+  ) => {
+    const currentX = Math.random() * 2 * TRANSLATE_RANGE - TRANSLATE_RANGE;
+    const currentY = Math.random() * 2 * TRANSLATE_RANGE - TRANSLATE_RANGE;
+
+    const distance = Math.sqrt(
+      Math.pow(previousX - currentX, 2) + Math.pow(previousY - currentY, 2)
+    );
+
+    await animation.start(
+      {
+        translateX: `${currentX}%`,
+        translateY: `${currentY}%`,
+      },
+      { duration: distance / 20, ease: 'linear' }
+    );
+    startAnimation(currentX, currentY);
+  };
+
+  useEffect(startAnimation as () => void, []);
 
   return (
-    <Fragment>
-      <motion.div
-        data-label="PermanentCloud1"
-        css={css`
-          width: ${width};
+    <motion.div
+      data-label={`Cloud${index}`}
+      css={css`
+        width: ${width};
 
-          z-index: ${zIndex++};
-          position: absolute;
-          left: 5%;
+        z-index: ${baseZIndex + index};
+        position: absolute;
+        left: ${left};
 
-          opacity: 0.8;
-        `}
-        style={{ top: getTop(landingScrollPercent, [10, 75]) }}
-      >
-        <Cloud width={width} />
-      </motion.div>
-    </Fragment>
+        opacity: 0.8;
+      `}
+      animate={animation}
+      style={{ top: getTop(landingScrollPercent, [top, '80%']) }}
+    >
+      <SvgCloud width={width} />
+    </motion.div>
   );
 };
+
+export default () => (
+  <Fragment>
+    {[
+      { left: '5%', top: '10%' },
+      { left: '80%', top: '15%' },
+    ].map((props, index) => (
+      <Cloud key={index} index={index} {...props} />
+    ))}
+  </Fragment>
+);

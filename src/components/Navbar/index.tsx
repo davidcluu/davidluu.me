1a59b347d18e3faaa4f9d66a59515201a2c22bbb
David Luu <luu.david.c@gmail.com>
2022-04-01 10:41:17 -0700

    Workaround for react-dark-mode-toggle-2 not working in SSR

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -11,6 +11,7 @@ import IDs from './ids';
 import { useAppDispatch, useAppSelector } from '../../store/hooks';
 import { Theme, actions } from '../../store/slices/Theme';
 import { getDarkMode } from '../../store/slices/Theme/selectors';
+import ClientOnly from '../ClientOnly';
 
 interface NavbarProps {
   wrapperClassName?: string;
@@ -22,6 +23,58 @@ interface NavbarProps {
   sublinkActiveClassName?: string;
 }
 
+const ThemeToggle = () => {
+  const isDarkMode = useAppSelector(getDarkMode);
+  const dispatch = useAppDispatch();
+
+  return (
+    <ClassNames>
+      {({ css, cx, theme: { utils } }) => {
+        const navbarHeight = utils.getThemeInvariantCSSValue(
+          'navbar.desktop.height',
+          utils.cssValueTransformers.pixelToNumber
+        );
+
+        const toggleHeight = navbarHeight * 0.6;
+        const toggleWidth = toggleHeight * 2;
+
+        const toggleClassName = css`
+          margin: 0 0.5em;
+
+          display: inline;
+        `;
+
+        const placeholderClassName = cx(
+          toggleClassName,
+          css`
+            width: ${toggleWidth}px;
+            height: ${toggleHeight}px;
+          `
+        );
+
+        return (
+          <ClientOnly
+            PlaceholderComponent={() => (
+              <div className={placeholderClassName} />
+            )}
+          >
+            <DarkModeToggle
+              isDarkMode={isDarkMode}
+              onChange={() =>
+                dispatch(
+                  actions.setTheme(isDarkMode ? Theme.Light : Theme.Dark)
+                )
+              }
+              size={`${toggleWidth}px`}
+              className={toggleClassName}
+            />
+          </ClientOnly>
+        );
+      }}
+    </ClassNames>
+  );
+};
+
 export default ({
   wrapperClassName,
   NavigationLink = (props) => <DefaultNavigationLink {...props} />,
@@ -31,9 +84,6 @@ export default ({
   sublinkClassName,
   sublinkActiveClassName,
 }: NavbarProps) => {
-  const isDarkMode = useAppSelector(getDarkMode);
-  const dispatch = useAppDispatch();
-
   const NavigationSublink = styled(NavigationLink)`
     font-size: 1em;
   `;
@@ -112,27 +162,7 @@ export default ({
                 Resume
               </NavigationLink>
             </div>
-            <DarkModeToggle
-              isDarkMode={isDarkMode}
-              onChange={() =>
-                dispatch(
-                  actions.setTheme(isDarkMode ? Theme.Light : Theme.Dark)
-                )
-              }
-              size={`${
-                2 *
-                0.6 *
-                utils.getThemeInvariantCSSValue(
-                  'navbar.desktop.height',
-                  utils.cssValueTransformers.pixelToNumber
-                )
-              }px`}
-              className={css`
-                margin: 0 0.5em;
-
-                display: inline;
-              `}
-            />
+            <ThemeToggle />
           </div>
         </nav>
       )}
